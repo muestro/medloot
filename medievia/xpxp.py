9edecde56a5b98d685a90425467883f93a5c7b59
Scott Sullivan <muestro@gmail.com>
2014-04-30 01:08:45 -0400

    Added timezone info to xpxp calculator.  Added 3rd party library pytz.

--- medievia/xpxp.py
+++ medievia/xpxp.py
@@ -1,4 +1,5 @@
-import datetime
+from datetime import datetime
+from pytz.gae import pytz
 
 
 def parse(moon=None, eclipse=None, full_text=None):
@@ -19,11 +20,13 @@ def parse(moon=None, eclipse=None, full_text=None):
                 break
 
     if moon_time and eclipse_time:
-        now = datetime.datetime.now()
-        xpxp = "{0} {1} {2}".format((eclipse_time - now).days + 2, (moon_time - now).days + 2, "2")
-        locate_serpents = "{0} {1} {2}".format((eclipse_time - now).days + 1, (moon_time - now).days + 2, "79")
-        create_rainstorm = "{0} {1} {2}".format((eclipse_time - now).days + 3, (moon_time - now).days + 1, "63")
-        remove_storm = "{0} {1} {2}".format((eclipse_time - now).days + 3, (moon_time - now).days + 6, "90")
+        current_time = datetime.utcnow().replace(tzinfo=pytz.utc)
+        moon_time_diff = moon_time - current_time
+        eclipse_time_diff = eclipse_time - current_time
+        xpxp = "{0} {1} {2}".format(eclipse_time_diff.days + 2, moon_time_diff.days + 2, "2")
+        locate_serpents = "{0} {1} {2}".format(eclipse_time_diff.days + 1, moon_time_diff.days + 2, "79")
+        create_rainstorm = "{0} {1} {2}".format(eclipse_time_diff.days + 3, moon_time_diff.days + 1, "63")
+        remove_storm = "{0} {1} {2}".format(eclipse_time_diff.days + 3, moon_time_diff.days + 6, "90")
 
         return {'xpxp': xpxp,
                 'locate_serpents': locate_serpents,
@@ -42,7 +45,8 @@ def _parse_moon(input_string):
     input_string = input_string.strip()
     input_string = input_string.split("fae clams making frae")[0]
     input_string = input_string.strip()
-    return datetime.datetime.strptime(input_string, "%a %b %d %H:%M:%S %Y")
+    dt = datetime.strptime(input_string, "%a %b %d %H:%M:%S %Y")
+    return _eastern_to_utc(dt)
 
 
 def _parse_eclipse(input_string):
@@ -54,4 +58,14 @@ def _parse_eclipse(input_string):
     input_string = input_string.strip()
     input_string = input_string.split("fae clams making stae")[0]
     input_string = input_string.strip()
-    return datetime.datetime.strptime(input_string, "%a %b %d %H:%M:%S %Y")
+    dt = datetime.strptime(input_string, "%a %b %d %H:%M:%S %Y")
+    return _eastern_to_utc(dt)
+
+
+def _eastern_to_utc(dt):
+    # assume the passed in time is Eastern, just not decorated that way
+    eastern = pytz.timezone('US/Eastern')
+    eastern_dt = eastern.localize(dt)
+
+    utc_dt = pytz.utc.normalize(eastern_dt.astimezone(pytz.utc))
+    return utc_dt
