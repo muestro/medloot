1d72109b9789f917a63b7808c1c292ed64113ac0
muestro <muestro@gmail.com>
2015-04-30 22:57:37 -0400

    Updated filter js and html.

--- static/search.js
+++ static/search.js
@@ -1,5 +1,25 @@
+function hideRows(rows, isActive){
+    rows.each(function(index){
+        var hideCount = 0;
+        if (this.hasAttribute('hideCount')){
+            hideCount = parseInt($(this).attr('hideCount'));
+        }
+
+        if(isActive){
+            $(this).hide();
+            hideCount += 1;
+            $(this).attr('hideCount', hideCount);
+        }else{
+            hideCount -= 1;
+            if(hideCount == 0){
+                $(this).show();
+            }
+            $(this).attr('hideCount', hideCount);
+        }
+    });
+}
+
 $(document).ready(function(){
-    // fill in the dynamic filters
     // equipable location
     var container = $("#equipableLocationFilterGroup");
     var equipableLocationSet = {};
@@ -17,62 +37,60 @@ $(document).ready(function(){
         container.append('<button equipableLocationFilterButton filter="' + value + '">' + value + '</button>');
     });
 
-    $("button[filter]").filter('[classFilterButton]').click(function() {
+    // dynamic filter building
+    $('.dynamicFilterGroup').each(function(){
+        var buttonGroupContainer = $(this).find('.filterButtonGroup');
+        var valueSet = {};
+        var filterGroupValue = $(this).attr('filterGroupValue');
+        $('td[filterGroup="' + filterGroupValue + '"][hasData]').each(function(){
+            // if it has a value, add it to our filter set
+            var value = $(this).attr("filter");
+            if(value && value != "" && !(value in valueSet)) {
+                valueSet[value] = true;
+            }
+        });
+        // setup each of the filter button groups
+        $.each(Object.keys(valueSet), function(index, value){
+            buttonGroupContainer.append('<button statFilterButton filter="' + value + '">' + value + '</button>');
+        });
+        if (Object.keys(valueSet).length > 0){
+            $(this).css('display', 'table-row');
+        }
+    });
+
+    var filterButtons = $("button[filter]");
+
+    filterButtons.filter('[statFilterButton]').click(function() {
+        $(this).toggleClass("active");
+        var isActive = $(this).hasClass('active');
+
+        var filter = $(this).attr('filter');
+        var rowsToHide = $('td[filter="' + filter + '"]:not([hasData])').closest('tr');
+
+        hideRows(rowsToHide, isActive);
+    });
+
+    filterButtons.filter('[classFilterButton]').click(function() {
         $(this).toggleClass("active");
         var isActive = $(this).hasClass('active');
 
         var filterText = $(this).attr('filter');
         var rowsToHide = $("span:contains(" + filterText + ")").filter('.lit').parents('tr.resultRow');
 
-        rowsToHide.each(function(index){
-            var hideCount = 0;
-            if (this.hasAttribute('hideCount')){
-                hideCount = parseInt($(this).attr('hideCount'));
-            }
-
-            if(isActive){
-                $(this).hide();
-                hideCount += 1;
-                $(this).attr('hideCount', hideCount);
-            }else{
-                hideCount -= 1;
-                if(hideCount == 0){
-                    $(this).show();
-                }
-                $(this).attr('hideCount', hideCount);
-            }
-        });
-
+        hideRows(rowsToHide, isActive);
     });
 
-    $("button[numberFilterButton]").click(function(){
+    filterButtons.filter('[numberFilterButton]').click(function() {
         $(this).toggleClass("active");
         var isActive = $(this).hasClass('active');
 
         var filter = $(this).attr('filter');
-        var rowsToHide = $('td.statBlock[filter="' + filter + '"]').not(':has(span)').parents('tr.resultRow');
+        var rowsToHide = $('td.statBlock[filter="' + filter + '"]:not([hasData])').closest('tr');
 
-        rowsToHide.each(function(index){
-            var hideCount = 0;
-            if (this.hasAttribute('hideCount')){
-                hideCount = parseInt($(this).attr('hideCount'));
-            }
-
-            if(isActive){
-                $(this).hide();
-                hideCount += 1;
-                $(this).attr('hideCount', hideCount);
-            }else{
-                hideCount -= 1;
-                if(hideCount == 0){
-                    $(this).show();
-                }
-                $(this).attr('hideCount', hideCount);
-            }
-        });
+        hideRows(rowsToHide, isActive);
     });
 
-    $("button[equipableLocationFilterButton]").click(function(){
+    filterButtons.filter('[equipableLocationFilterButton]').click(function(){
         $(this).toggleClass("active");
         var isActive = $(this).hasClass('active');
 
@@ -81,23 +99,6 @@ $(document).ready(function(){
             return $(this).text().indexOf(filter) == -1;
         }).parents('tr.resultRow');
 
-        rowsToHide.each(function(index){
-            var hideCount = 0;
-            if (this.hasAttribute('hideCount')){
-                hideCount = parseInt($(this).attr('hideCount'));
-            }
-
-            if(isActive){
-                $(this).hide();
-                hideCount += 1;
-                $(this).attr('hideCount', hideCount);
-            }else{
-                hideCount -= 1;
-                if(hideCount == 0){
-                    $(this).show();
-                }
-                $(this).attr('hideCount', hideCount);
-            }
-        });
+        hideRows(rowsToHide, isActive);
     });
 });
